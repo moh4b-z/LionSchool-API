169f751ba6ae207b4772b20cb8cea6a2b33bffa4
Mohammad Salim.S.S <77904933+moh4b-z@users.noreply.github.com>
2024-12-06 12:06:23 -0300

    Update functions.js

--- module/functions.js
+++ module/functions.js
@@ -19,11 +19,11 @@ function alunosLista(){
 
 // Recupera informações de um aluno específico com base no número de matrícula
 function infoAluno(nubMatricula){
-    let matricula = nubMatricula
+    let matricula = String(nubMatricula.toUpperCase())
     let objetoRetorno = false
 
     listaAlunos.forEach(function(aluno){
-        if(aluno.matricula == matricula){
+        if(String(aluno.matricula.toUpperCase()) == matricula){
             objetoRetorno = aluno
         }
     })
@@ -33,13 +33,13 @@ function infoAluno(nubMatricula){
 
 // Recupera uma lista de todos os alunos matriculados no curso especificado. DS ou REDES 
 function alunosCurso(nomeCurso){
-    let nCurso = nomeCurso
+    let nCurso = String(nomeCurso.toUpperCase())
     let objetoRetorno = {curso : nCurso, alunos : []}
 
-    if(listaCursos.some(curso => curso.sigla === nCurso)){
+    if(listaCursos.some(curso => String(curso.sigla.toUpperCase()) === nCurso)){
         listaAlunos.forEach(function(aluno){
             aluno.curso.forEach(function(CursosDoAluno){
-                if(CursosDoAluno.sigla == nCurso){
+                if(String(CursosDoAluno.sigla.toUpperCase()) == nCurso){
                     objetoRetorno.alunos.push(aluno)
                 }
             })
@@ -53,12 +53,12 @@ function alunosCurso(nomeCurso){
 
 // Recupera uma lista de todos os alunos com o status especificado. Finalizado ou Cursando
 function statusCurso(statusAlunosCursoS){
-    let statusAC = statusAlunosCursoS
+    let statusAC = String(statusAlunosCursoS.toUpperCase())
     let objetoRetorno = { status : statusAC, alunos : []}
-    if(listaAlunos.some(aluno => aluno.status === statusAC)){
+    if(listaAlunos.some(aluno => String(aluno.status.toUpperCase()) === statusAC)){
         if(Array.isArray(listaAlunos)){
             listaAlunos.forEach(function(aluno){
-                if(aluno.status == statusAC){
+                if(String(aluno.status.toUpperCase()) == statusAC){
                     objetoRetorno.alunos.push(aluno)
                 }
             })
@@ -75,21 +75,21 @@ function statusCurso(statusAlunosCursoS){
 
 // Recupera uma lista de alunos matriculados em um curso especificado e com base em um status da disciplina Aprovado, Reprovado ou EXAME
 function alunosStatusCurso(nomeCurso, statusAluno){
-    let nCurso = nomeCurso
-    let statusA = statusAluno
+    let nCurso = String(nomeCurso.toUpperCase())
+    let statusA = String(statusAluno.toUpperCase())
     let objetoRetorno = { curso : nCurso, status : statusA, alunos : []}
 
 
-    if(listaCursos.some(curso => curso.sigla === nCurso)){
+    if(listaCursos.some(curso => String(curso.sigla.toUpperCase()) === nCurso)){
         listaAlunos.forEach(function(aluno){
             let alunoRetorno = aluno
             let disciplinasStaus = []
             
             alunoRetorno.curso.forEach(function(CursosDoAluno){
                 let cursoAluno = CursosDoAluno
-                if(CursosDoAluno.sigla == nCurso){
+                if(String(CursosDoAluno.sigla.toUpperCase()) == nCurso){
                     CursosDoAluno.disciplinas.forEach(function(disciplinaCurso){
-                        if(disciplinaCurso.status == statusA){
+                        if(String(disciplinaCurso.status.toUpperCase()) == statusA){
                             disciplinasStaus.push(disciplinaCurso)
                         }
                         cursoAluno.disciplinas = disciplinasStaus
@@ -107,27 +107,27 @@ function alunosStatusCurso(nomeCurso, statusAluno){
 }
 
 // Recupera uma lista de alunos matriculados em um curso especificado e com base no ano de conclusão 
-function alunosCursoDeAno(nomeCurso, anoDeComnclusao){
-    let nCurso = nomeCurso
-    let anoDC = anoDeComnclusao
-    let objetoRetorno = { curso : nCurso, anoDeComnclusao : anoDC, alunos : []}
+function alunosCursoDeAno(nomeCurso, anoDeConclusao) {
+    let nCurso = nomeCurso.toUpperCase()
+    let anoDC = anoDeConclusao
+    let objetoRetorno = { curso: nCurso, anoDeConclusao: anoDC, alunos: [] }
 
-    
-
-    if(listaCursos.some(curso => curso.sigla === nCurso)){
-        listaAlunos.forEach(function(aluno){        
-            aluno.curso.forEach(function(CursosDoAluno){
-                if(CursosDoAluno.sigla == nCurso && CursosDoAluno.conclusao == anoDC){
-                    objetoRetorno.alunos.push(aluno)                
+    if (listaCursos.some(curso => curso.sigla === nCurso)) {
+        listaAlunos.forEach(function(aluno) {
+            aluno.curso.forEach(function(CursosDoAluno) {
+                if (CursosDoAluno.sigla.toUpperCase() === nCurso && CursosDoAluno.conclusao == anoDC) {
+                    objetoRetorno.alunos.push(aluno)
                 }
             })
         })
-    }else{
-        objetoRetorno = false
+    } else {
+        return false
     }
 
     return objetoRetorno
 }
+
+
 
 
 function filtro(statusAlunosCursoS, nomeCurso, statusAluno, anoDeComnclusao) {
@@ -149,9 +149,10 @@ function filtro(statusAlunosCursoS, nomeCurso, statusAluno, anoDeComnclusao) {
 }
 
 // console.log(alunosCurso("DS"))
-// console.log(alunosStatusCurso("DS", "EXAME"))
-// console.log(alunosCursoDeAno("DS", "2022"))
-// console.log(filtro("", "DS","","2022"))
+// console.log(alunosStatusCurso("Ds", "EXaME"))
+// console.log(alunosCursoDeAno("ds", "2022"))
+// console.log(filtro("", "ds","","2022"))
+// console.log(filtro("", "ds","aprovado",""))
 
 module.exports = {
     cursosLista,
